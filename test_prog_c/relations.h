11831eec6dfaba4e4d74ea07ed746c7c28501bae
CISSE <daoudacisse750@gmail.com>
2025-07-16 17:37:02 +0000

    Mise à jour des prototypes et struct dans relations.h

--- test_prog_c/relations.h
+++ test_prog_c/relations.h
@@ -1,7 +1,8 @@
 #include "gestion_des_classes/gestion_classes.h"
 #include "gestion_matiere/matiere.h"
+typedef struct Se_faire Se_faire;
 
-void associer_matiere_classe(int class_code, int mat_reference, Classe classes[], int *size, Se_faire tab[], int *se_faire_size);
-void initialize_se_faire(Se_faire tab[], int *se_faire_size);
+void associer_matiere_classe(int class_code, int mat_reference, Classe classes[], int *size, Se_faire tab[], int *Se_faire_size);
+void initialize_Se_faire(Se_faire tab[], int *Se_faire_size);
 int rechercher_association(int target_classe, int target_matiere);
 void dissocier_matiere_classe(int target_classe, int target_matiere);
