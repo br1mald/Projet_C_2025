975cafe3f479a764ecc880a60492996f63195aa0
CISSE <daoudacisse750@gmail.com>
2025-07-16 17:32:52 +0000

    Correction du type de coefficient dans la structure matiere et mise à jour des prototypes

--- test_prog_c/gestion_matiere/matiere.h
+++ test_prog_c/gestion_matiere/matiere.h
@@ -7,11 +7,20 @@ typedef struct
     int short coefficient;
 }matiere;
 
+typedef struct 
+{
+    int reference_matiere;
+    int code_classe;
+}se_faire;
+
 int gestion_matiere();
 void ajouter_matiere();
 void modifier_matiere();
 int rechercher_matiere(int);
 void supprimer_matiere();
 void afficher_matiere();
+void associer_matiere_classe();
+void dissocier_matiere_classe();
+void afficher_matiere_classe();
 
 #endif
