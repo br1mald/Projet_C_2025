bf6596cbabed588d7f9a07b12eaf4020a658fcf8
CISSE <daoudacisse750@gmail.com>
2025-07-16 17:37:41 +0000

    Mise à jour des fonctions d'association/dissociation dans relations.c

--- test_prog_c/relations.c
+++ test_prog_c/relations.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include "relations.h"
 
-void associer_matiere_classe(int class_code, int mat_reference, Classe classes[], int *size, Se_faire tab[], int *se_faire_size) {
+void associer_matiere_classe(int class_code, int mat_reference, Classe classes[], int *size, Se_faire tab[], int *Se_faire_size) {
     if (rechercher_association(class_code, mat_reference) == 1) {
         printf("Matière et classe déjà associées.\n");
     } else if (search(classes, class_code, *size) != -1 && rechercher_matiere(mat_reference) == 1) {
-        tab[*se_faire_size].code = class_code;
-        tab[*se_faire_size].reference = mat_reference;
-        FILE *writer = fopen("gestion_des_classes/se_faire.csv", "a");
-        fprintf(writer, "%d,%d\n", tab[*se_faire_size].code, tab[*se_faire_size].reference);
+        tab[*Se_faire_size].code = class_code;
+        tab[*Se_faire_size].reference = mat_reference;
+        FILE *writer = fopen("gestion_des_classes/Se_faire.csv", "a");
+        fprintf(writer, "%d,%d\n", tab[*Se_faire_size].code, tab[*Se_faire_size].reference);
         fclose(writer);
         printf("Opération réussie.\n");
-        (*se_faire_size)++;
+        (*Se_faire_size)++;
     } else printf("Matière ou classe inexistante. Veuillez vérifier puis réessayer.\n");
 }
 
 int rechercher_association(int target_classe, int target_matiere) {
     int classe, matiere;
-    FILE *reader = fopen("gestion_des_classes/se_faire.csv", "r");
+    FILE *reader = fopen("gestion_des_classes/Se_faire.csv", "r");
     char buffer[50];
     while (fgets(buffer, sizeof(buffer), reader) != NULL) {
         int line = sscanf(buffer, "%d, %d", &classe, &matiere);
@@ -26,21 +26,21 @@ int rechercher_association(int target_classe, int target_matiere) {
     return 0;
 }
 
-void initialize_se_faire(Se_faire tab[], int *se_faire_size) {
-    FILE *reader = fopen("gestion_des_classes/se_faire.csv", "r");
+void initialize_Se_faire(Se_faire tab[], int *Se_faire_size) {
+    FILE *reader = fopen("gestion_des_classes/Se_faire.csv", "r");
     char buffer[50],level[10];
     while (fgets(buffer, sizeof(buffer), reader) != NULL) {
-        int line = sscanf(buffer, "%d,%d", &tab[*se_faire_size].code, &tab[*se_faire_size].reference);
-        (*se_faire_size)++;
+        int line = sscanf(buffer, "%d,%d", &tab[*Se_faire_size].code, &tab[*Se_faire_size].reference);
+        (*Se_faire_size)++;
     }
 }
 
 void dissocier_matiere_classe(int target_classe, int target_matiere) {
-    FILE *og_file = fopen("gestion_des_classes/se_faire.csv", "r");
+    FILE *og_file = fopen("gestion_des_classes/Se_faire.csv", "r");
     FILE *new_file = fopen("gestion_des_classes/temp.csv", "w");
     int classe_code, matiere_ref;
     char buffer[50];
-    const char *new_name = "gestion_des_classes/se_faire.csv";
+    const char *new_name = "gestion_des_classes/Se_faire.csv";
     while (fgets(buffer, sizeof(buffer), og_file) != NULL) {
         int line = sscanf(buffer, "%d,%d", &classe_code, &matiere_ref);
         if (classe_code != target_classe || matiere_ref != target_matiere) {
@@ -49,6 +49,6 @@ void dissocier_matiere_classe(int target_classe, int target_matiere) {
     }
     fclose(og_file);
     fclose(new_file);
-    remove("gestion_des_classes/se_faire.csv");
+    remove("gestion_des_classes/Se_faire.csv");
     rename("gestion_des_classes/temp.csv", new_name);
 }
